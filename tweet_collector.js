427a5739cc25df30b5970e7a868899a31d34b966
Michaël Nass - PlugN <michael.nass@free.fr>
2024-07-08 16:28:04 +0200

    Update tweet_collector.js

--- tweet_collector.js
+++ tweet_collector.js
@@ -1,30 +1,27 @@
 let i = 1;
 let dict = {};
-let like_regex = /\b(\d+) Like|Likes\b/;
-let retweet_regex = /\b(\d+) Retweet|Retweets\b/;
-let reply_regex = /\b(\d+) Repl(ies|y)\. Reply\b/;
-let time_regex =  /\btime datetime=\"(.*?)\">\b/;
-let status_regex = /\b\/status\/(\d+)\b/;
+let like_regex = /\b(\d+) J'aime\b/;
+let retweet_regex = /\b(\d+) reposts\b/;
+let reply_regex = /\b(\d+) réponses\b/;
+let views_regex = /\b(\d+) vues\b/;
             
 function loop() {      
     setTimeout(function() {
         window.scrollBy(0,1000);
-	let element = document.getElementsByClassName("css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu");
+	let element = document.getElementsByClassName("css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4");
         for(let index=0; index<element.length; index++) {
             try {
 	        let reply = element[index].innerHTML.match(reply_regex);
 	        let like = element[index].innerHTML.match(like_regex);
 	        let retweet = element[index].innerHTML.match(retweet_regex);
-	        let time = element[index].innerHTML.match(time_regex);
-	        let status = element[index].innerHTML.match(status_regex);
-	        dict[element[index].innerText.split("\n")[1].concat("-"+time[1])] = {
+		let views = element[index].innerHTML.match(views_regex);
+	        dict[element[index]] = {
 		    "like":`${like[1]}`,
 		    "reply":`${reply[1]}`,
 		    "retweet":`${retweet[1]}`,
-		    "time":`${time[1]}`,
-		    "text":`${element[index].innerText}`,
-		    "status":`${status[1]}`
+		    "views":`${views[1]}`,
 	        };
+		    console.log(dict)
            } 
            catch (error) {
 	       console.log(error)
