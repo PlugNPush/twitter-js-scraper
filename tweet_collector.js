f6b24da4c4ed9399cb9825ffcbbcff0f49bbc6a2
Michaël Nass - PlugN <michael.nass@free.fr>
2024-07-08 16:34:50 +0200

    Update tweet_collector.js

--- tweet_collector.js
+++ tweet_collector.js
@@ -4,18 +4,19 @@ let like_regex = /\b(\d+) J'aime\b/;
 let retweet_regex = /\b(\d+) reposts\b/;
 let reply_regex = /\b(\d+) réponses\b/;
 let views_regex = /\b(\d+) vues\b/;
-            
+let time_regex =  /\btime datetime=\"(.*?)\">\b/;
+
 function loop() {      
     setTimeout(function() {
         window.scrollBy(0,1000);
-	let element = document.getElementsByClassName("css-175oi2r r-1kbdv8c r-18u37iz r-1wtj0ep r-1ye8kvj r-1s2bzr4");
+	let element = document.getElementsByClassName("css-175oi2r r-18u37iz r-1udh08x r-1c4vpko r-1c7gwzm r-o7ynqc r-6416eg r-1ny4l3l r-1loqt21");
         for(let index=0; index<element.length; index++) {
             try {
 	        let reply = element[index].innerHTML.match(reply_regex);
 	        let like = element[index].innerHTML.match(like_regex);
 	        let retweet = element[index].innerHTML.match(retweet_regex);
 		let views = element[index].innerHTML.match(views_regex);
-	        dict[index] = {
+	        dict[element[index].innerText.split("\n")[1].concat("-"+time[1])] = {
 		    "like":`${like[1]}`,
 		    "reply":`${reply[1]}`,
 		    "retweet":`${retweet[1]}`,
